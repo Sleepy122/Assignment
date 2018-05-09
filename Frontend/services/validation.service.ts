cd37cf3576c6f8b9a818911d4b4d5660d6a4912c
Sleepy122 <Calder.Jones3155@gmail.com>
2018-05-09 09:46:03 -0500

    Update validation.service.ts

--- Frontend/services/validation.service.ts
+++ Frontend/services/validation.service.ts
@@ -39,9 +39,9 @@ export class ValidationService {
   isAStateCodeValidator(item:string){
     let stateList = new Array("AK","AL","AR","AZ","CA","CO","CT","DC","DE","FL","GA","GU","HI","IA","ID", "IL","IN","KS","KY","LA","MA","MD","ME","MH","MI","MN","MO","MS","MT","NC","ND","NE","NH","NJ","NM","NV","NY", "OH","OK","OR","PA","PR","PW","RI","SC","SD","TN","TX","UT","VA","VI","VT","WA","WI","WV","WY");
     if(stateList.indexOf(item)==-1){
-      return false;
+      return {notAStateValidator:true};
     }else{
-      return true;
+      return null;
     }
 
   }
