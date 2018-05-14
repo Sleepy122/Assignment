5ab1bace7d0b64670cfa36f82df21d42e6c44465
Sleepy122 <Calder.Jones3155@gmail.com>
2018-05-14 09:14:55 -0500

    Update InventoryDaoImpl.java

--- Backend/main/java/com/bnsf/inventory/dao/InventoryDaoImpl.java
+++ Backend/main/java/com/bnsf/inventory/dao/InventoryDaoImpl.java
@@ -1,9 +1,6 @@
 package com.bnsf.inventory.dao;
 import java.util.List;
-
-import org.hibernate.Query;
-import org.hibernate.Session;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,40 +15,27 @@ public class InventoryDaoImpl implements InventoryDao {
 		Session s = HibernateUtil.getSession();
 		Query q = s.createQuery("from InventoryItem");
 		List<InventoryItem> items = q.list();
-		for(int i = 0;i<items.size();i++) {
-		//System.out.print(items.get(i).toString()+"_____________________________________________________________________________________________");
-		}
 		s.close();
 		return items;
 	}
 	
-	public void updateInventory(InventoryWrapper WrappedNewItem) {
+	public void updateInventory(InventoryItem wrappedNewItem) {
 		//creating an instance of inventory item from my wrapper class
-		InventoryItem newItem = new InventoryItem(WrappedNewItem.eqpInit,WrappedNewItem.eqpNumb,WrappedNewItem.stn333,WrappedNewItem.stnSt,WrappedNewItem.dest333,WrappedNewItem.destSt,
-				WrappedNewItem.etnTs,WrappedNewItem.eqpLgth,WrappedNewItem.eqpWdth,WrappedNewItem.eqpWeight,WrappedNewItem.ecsId,WrappedNewItem.ecsPrcsd, WrappedNewItem.statCd);
 		Session s = HibernateUtil.getSession();
 		Transaction tx = s.beginTransaction();
-		Query q = s.createQuery("from InventoryItem I where I.Identifier2 = :var");
-		q.setParameter("var", Integer.parseInt(WrappedNewItem.identifier2));
-		List<InventoryItem> results = q.list();
-		InventoryItem item = (InventoryItem) results.get(0);
-		s.delete(item);
-		s.persist(newItem);
+		s.saveOrUpdate(wrappedNewItem);
 		tx.commit();
 		s.close();
 		
 	}
 	
 	public void deleteInventory(String pk) {
-		int newPk=-5;
-		System.out.print(pk+"_____________________________________________________________________________________________");
-		newPk = Integer.parseInt(pk);
+		 int newPk = Integer.parseInt(pk);
 		Session s = HibernateUtil.getSession();
 		Transaction tx = s.beginTransaction();
 		Query q = s.createQuery("from InventoryItem I where I.Identifier2 = :var");
 		q.setParameter("var", newPk);
 		List<InventoryItem> results = q.list();
-		System.out.println("*****");
 		System.out.println(results.get(0).toString());
 		InventoryItem item = (InventoryItem) results.get(0);		
 		s.delete(item);
@@ -60,23 +44,10 @@ public class InventoryDaoImpl implements InventoryDao {
 		
 	}
 	
-	public void createInventory(InventoryWrapper WrappedNewItem) {
+	public void createInventory(InventoryItem wrappedNewItem) {
 		Session s = HibernateUtil.getSession();
-		//creating an instance of inventory item from my wrapper class
-		InventoryItem newItem = new InventoryItem(WrappedNewItem.eqpInit,WrappedNewItem.eqpNumb,WrappedNewItem.stn333,WrappedNewItem.stnSt,WrappedNewItem.dest333,WrappedNewItem.destSt,
-				WrappedNewItem.etnTs,WrappedNewItem.eqpLgth,WrappedNewItem.eqpWdth,WrappedNewItem.eqpWeight,WrappedNewItem.ecsId,WrappedNewItem.ecsPrcsd, WrappedNewItem.statCd);
 		Transaction tx = s.beginTransaction();
-		s.saveOrUpdate(newItem);
-		tx.commit();
-		s.close();
-	}
-	
-	public void testing(InventoryItem thing) {
-		Session s = HibernateUtil.getSession();
-		
-		System.out.print(thing.toString()+"_____________________________________________________________________________");
-		Transaction tx = s.beginTransaction();
-		s.save(thing);
+		s.saveOrUpdate(wrappedNewItem);
 		tx.commit();
 		s.close();
 	}
